5a32f815ce4e3507fe9c6b35e41c082d18292700
Nitish K S <[nitishsgk@gmail.com]>
2022-05-29 22:37:28 +0530

    Change in ticket fare display

--- rail-app/src/navbar/nav-bar.js
+++ rail-app/src/navbar/nav-bar.js
@@ -1,10 +1,13 @@
-import './nav.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import img from "../img/logo.png"
+import './nav.css';
+
 
 const Navbar = () => {
   return (
     <div className="nav-bar">
+    {/* <img src={img} className="img-size"></img> */}
     <FontAwesomeIcon icon={faHome} size="lg" color='white' />
     </div>
   );
