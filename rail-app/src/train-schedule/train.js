3975adbc7efb204265343da9786d14bd62f73bd3
Nitish K S <[nitishsgk@gmail.com]>
2022-06-01 11:44:43 +0530

    More styling

--- rail-app/src/train-schedule/train.js
+++ rail-app/src/train-schedule/train.js
@@ -1,4 +1,3 @@
-import "./train-dt.css"
 import { useLocation } from "react-router-dom"
 
 
@@ -14,8 +13,8 @@ const TrainDetail = () => {
         class: ["Sitting", "Sleeper", "AC-Sleeper", "AC", "First"]
     }
     return (
-        <div className="container train-container">
-            <div className="value-container">{train.id} - {train.name}</div>
+        <div>
+        <div className="value-container">{train.id} - {train.name}</div>
             <div className="to-from">
                 <div>{train.from} {train.arrivalTime}</div>
                 <div>{train.to} {train.reachTime}</div>
