35f85cd53374f6221adceabb52ed57541ef3c029
Nitish K S <[nitishsgk@gmail.com]>
2022-06-06 01:33:41 +0530

    Styling and train js

--- db/create.sql
+++ db/create.sql
@@ -1,22 +1,23 @@
 create database if not exists rail;
 use rail;
--- drop table if exists passenger;
 drop table if exists fare_table;
 drop table if exists tickets;
+drop table if exists fare_tables;
 drop table if exists t_schedule;
 drop table if exists train_details;
+drop table if exists passenger;
 
 
 create table if not exists passenger(username varchar(25) primary key, aadhaar numeric unique not null, name varchar(25) not null, age numeric not null, gender char(1) not null, address varchar(50) not null, mobile numeric not null, password varchar(25) not null, email varchar(25) not null, check(age >= 18));
-create table if not exists train_details(train_no varchar(25) primary key, train_name varchar(25) not null, from_station varchar(25) not null, to_station varchar(25) not null, total_coach numeric not null default 4, total_seats numeric not null, booked_seats numeric not null default 0, FC_total numeric not null, FC_booked numeric not null default 0, AC_total numeric not null, AC_booked numeric not null default 0, ST_total numeric not null, ST_booked numeric not null default 0, SL_total numeric not null, SL_booked numeric not null default 0, check(total_seats >= booked_seats), check(FC_total >= FC_booked), check(AC_total >= AC_booked), check(ST_total >= ST_booked), check(SC_total >= SC_booked));
+create table if not exists train_details(train_no varchar(25) primary key, train_name varchar(25) not null, from_station varchar(25) not null, to_station varchar(25) not null, total_coach numeric not null, total_seats numeric not null, booked_seats numeric not null default 0);
 create table if not exists fare_table(station_id varchar(25) not null, train_no varchar(25) not null, fare numeric not null, foreign key (train_no) references train_details(train_no));
 create table if not exists tickets(ticket_no varchar(25) primary key, username varchar(25), profile_name varchar(25) not null, age numeric not null, gender char(1) not null, mobile numeric not null, date_ date not null, train_no varchar(25) not null, coach_no varchar(25) not null, seat_no numeric not null, from_station varchar(25) not null, to_station varchar(25) not null, fare numeric not null, foreign key (train_no) references train_details(train_no));
 create table if not exists t_schedule(station_name varchar(25) not null, platform_no numeric not null, train_no varchar(25) not null, arrival timestamp not null, departure timestamp not null, foreign key (train_no) references train_details(train_no));
 
-insert into train_details values("12163", "Chennai Express", "Delhi - DLI", "Chennai Central - MAS", 8, 300, 0, 50, 0, 50, 0, 100, 0, 100, 0);
-insert into train_details values("12632", "Nellai Express", "Thirunelveli - TEN", "Chennai Central - MAS", 12, 450, 0, 75, 0, 75, 0, 150, 0, 150, 0);
-insert into train_details values("12635", "Nellai Express", "Chennai Central - MAS", "Madurai Jn - MDU", 8, 300, 0, 50, 0, 50, 0, 100, 0, 100, 0);
-insert into train_details values("16859", "Mangalore Express", "Chennai Central - MAS", "Mangalore Central - MAQ", 12, 450, 0, 75, 0, 75, 0, 150, 0, 150, 0);
+insert into train_details values("12163", "Chennai Express", "Delhi - DLI", "Chennai Central - MAS", 10, 300, 0);
+insert into train_details values("12632", "Nellai Express", "Thirunelveli - TEN", "Chennai Central - MAS", 15, 450, 0);
+insert into train_details values("12635", "Nellai Express", "Chennai Central - MAS", "Madurai Jn - MDU", 10, 300, 0);
+insert into train_details values("16859", "Mangalore Express", "Chennai Central - MAS", "Mangalore Central - MAQ", 15, 450, 0);
 
 insert into t_schedule values( "Delhi - DLI", 1, "12163", '2022-05-30 06:00:00', '2022-05-30 06:10:00'); 
 insert into t_schedule values( "Delhi", 2, "12163", '2022-05-30 06:40:00', '2022-05-30 06:50:00'); 
@@ -28,5 +29,10 @@ insert into t_schedule values( "Delhi", 7, "12163", '2022-05-30 10:00:00', '2022
 insert into t_schedule values( "Delhi", 8, "12163", '2022-05-30 10:40:00', '2022-05-30 10:50:00'); 
 insert into t_schedule values( "mangalore", 9, "12635", '2022-05-30 11:20:00', '2022-05-30 11:30:00');
 insert into t_schedule values( "Chennai Central - MAS", 10, "12163", '2022-05-30 12:00:00', '2022-05-30 12:00:00'); 
+
+insert into passenger values ("Satty",4394309,"Sathya Naarayanaa",25,"M","Kalpakkam",9445446158,"sathya","satty@gmail.com");
+insert into passenger values ("Hothands",5994309,"Sudeep K",27,"M","Coimbatore",9445454158,"sudeep","sudeep@gmail.com");
+insert into passenger values ("mrHope",4394789,"Nitish K S",47,"M","Chennai",7200838028,"nitish","niti@gmail.com");
+
 select * from train_details;
 select * from t_schedule;
---
+++ rail-app/src/train-schedule/trains.js
@@ -0,0 +1,30 @@
+import { ReactSession } from "react-client-session";
+import { useNavigate } from "react-router-dom";
+import Train from "./train";
+ReactSession.setStoreType("sessionStorage");
+
+const TrainDetail = (props) => {
+    const trainDetail = props.trainDetail;
+    const userFetch = props.userFetch;
+    const navigate = useNavigate();
+    const username = ReactSession.get("username");
+    console.log(userFetch.from);
+    // console.log("The Train Details to be displayed: ",props.train_det);
+    const handleSubmit = () => {
+        if (username) {
+            navigate("/booking");
+        } else {
+            alert("Login first");
+            navigate("/login");
+        }
+    };
+    return (
+        <>
+            {trainDetail.map((train) => (
+               <Train train={train} handleSubmit={handleSubmit}/>
+            ))}
+        </>
+    );
+};
+
+export default TrainDetail;
